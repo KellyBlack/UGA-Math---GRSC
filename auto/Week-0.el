29136f7051919e7dec3aabf95c5d6e9ee7e02bc4
William Edward Olsen <Billy@Shelbys-MacBook-Pro.local>
2019-06-24 10:51:48 -0400

    uploading notes for Kelly Black

--- auto/Week-0.el
+++ auto/Week-0.el
@@ -1,15 +1,22 @@
 (TeX-add-style-hook
  "Week-0"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "sec:introduction"
-    "sec:daily-discussions"))
+    "sec:structure-course"
+    "itm:DP"
+    "sec:nitty-gritty-day"
+    "itm:first"
+    "itm:second"
+    "sec:daily-discussions"
+    "sec:guest-speakers"
+    "sec:preparing-your-first"))
  :latex)
 
--- auto/Week-1.el
+++ auto/Week-1.el
@@ -1,18 +1,16 @@
 (TeX-add-style-hook
  "Week-1"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "sec:week-1"
-    "sec:preparing-your-first"
     "sec:intr-grsc-7770"
-    "sec:covering-syllabus"
-    "sec:data-driven-teaching"))
+    "sec:covering-syllabus"))
  :latex)
 
